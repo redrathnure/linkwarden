b2a49a39ca60ccd14fa87dfa3a8039dd33928b20
Maxim Medvedev <redrathnure@gmail.com>
2025-03-19 20:54:55 +0100

    chore: no more plans around alpine images
    playwright does not supports non glibc based distros

--- docker/bin/docker-entrypoint.sh
+++ docker/bin/docker-entrypoint.sh
@@ -36,7 +36,7 @@ if [ "$PUID" != "$ORIGIN_NODE_UID" -o "$PGID" != "$ORIGIN_NODE_GID" ]; then
         #chown node:node -R $(ls $SRV_DATA_ROOT -I data)
         # Pay attention to playwright and /data/.next/cache files
         chown node:node -R $PLAYWRIGHT_BROWSERS_PATH
-        chown node:node $SRV_DATA_ROOT/.next/cache   
+        chown node:node $SRV_DATA_ROOT/.next/cache
     else
         printf "WARNING: PUID/PGID envs were specified together with custom -u UID:GID argument.\n"
         printf "This may lead to unexpected problems with file permissions.\n"
