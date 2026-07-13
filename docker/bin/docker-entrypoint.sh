4856bcca5bc89901c4c1f692ac5a745c38877cf5
Maxim Medvedev <redrathnure@gmail.com>
2026-07-13 08:38:21 +0200

    fix: apps/web/.next/cache permissions

--- docker/bin/docker-entrypoint.sh
+++ docker/bin/docker-entrypoint.sh
@@ -35,6 +35,7 @@ if [ "$PUID" != "$ORIGIN_NODE_UID" -o "$PGID" != "$ORIGIN_NODE_GID" ]; then
         #chown node:node -R $(ls $SRV_DATA_ROOT -I data)
         # Pay attention to playwright and /data/.next/cache files
         chown node:node -R $PLAYWRIGHT_BROWSERS_PATH
+        chown node:node -R $SRV_DATA_ROOT/apps/web/.next
         #chown node:node -R $SRV_DATA_ROOT/apps/web/.next/cache 
     else
         printf "WARNING: PUID/PGID envs were specified together with custom -u UID:GID argument.\n"
